86d6d01adb8d7f92c6144f73b537dc9f66907866
Marc <konopka.marc@web.de>
2025-12-19 23:44:20 +0100

    Set up project structure and start menu layout

---
+++ script.js
@@ -0,0 +1,40 @@
+function init(){
+    let burgerMenu = document.getElementById('burger')
+
+    burgerMenu.innerHTML = `<svg
+  width="28"
+  height="28"
+  viewBox="0 0 28 28"
+  xmlns="http://www.w3.org/2000/svg"
+  fill="none"
+>
+  <line
+    x1="4"
+    y1="7"
+    x2="24"
+    y2="7"
+    stroke="#000000"
+    stroke-width="2.5"
+    stroke-linecap="round"
+  />
+  <line
+    x1="4"
+    y1="14"
+    x2="24"
+    y2="14"
+    stroke="#000000"
+    stroke-width="2.5"
+    stroke-linecap="round"
+  />
+  <line
+    x1="4"
+    y1="21"
+    x2="24"
+    y2="21"
+    stroke="#000000"
+    stroke-width="2.5"
+    stroke-linecap="round"
+  />
+</svg>
+`
+}
